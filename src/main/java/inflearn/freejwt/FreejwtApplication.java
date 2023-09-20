1ca41c525c0f46e49e599abeb45dbcfe8753d6f1
EUIJOON <euijoonism@gmail.com>
2023-09-20 19:34:19 +0900

    2.7.15 초기 세팅

---
+++ src/main/java/inflearn/freejwt/FreejwtApplication.java
@@ -0,0 +1,13 @@
+package inflearn.freejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreejwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreejwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inflearn/freejwt/FreejwtApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.freejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreejwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
