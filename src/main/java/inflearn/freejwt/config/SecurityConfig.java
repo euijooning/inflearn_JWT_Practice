30d17aa1ffb714ad1e03b34c20ce9caef408d040
EUIJOON <euijoonism@gmail.com>
2023-09-21 03:35:13 +0900

    SecurityConfig에 오버라이드 메서드 추가, User, Authority 엔티티 구현

--- src/main/java/inflearn/freejwt/config/SecurityConfig.java
+++ src/main/java/inflearn/freejwt/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package inflearn.freejwt.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,7 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/hello").permitAll() // /api/hello 에 대한 요청은 인증 없이 접근을 허용하겠다.
                 .anyRequest().authenticated(); // 나머지 요청들에 대해서는 인증을 받아야 한다.
         // 이렇게 하고 나서 get 요청 보내보면 401 에러 사라짐.
+    }
 
 
+    /**
+     * h2-console 하위 모든 요청들과 파비콘 관련 요청은
+     * Spring Security 로직을 수행하지 않고 접근할 수 있게 configure 메서드를 오버라이드해서 내용을 추가해준다.
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                );
     }
 }
---
+++ src/main/java/inflearn/freejwt/entity/User.java
@@ -0,0 +1,44 @@
+package inflearn.freejwt.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username", length = 50, unique = true)
+    private String username;
+
+    @JsonIgnore
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nickname", length = 50)
+    private String nickname;
+
+    @JsonIgnore
+    @Column(name = "activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable( // 다대다를 일대다, 다대일 관계로 재정의했다는 뜻.
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
+}
